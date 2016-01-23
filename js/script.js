da55c03be7fc36017711c9dc37a582027dc4dfff
Liz Suqiasyan <lizasuqiasyan84@mail.ru>
2016-01-23 11:48:00 +0400

    նկարների ֆայլի ներառում

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
 
-    document.forms[0].action="http://ru.aliexpress.com/";
-   /*function changeImage(){
-       var im = document.images[2];
-       im.src = "img/collection.jpg";
-       var normalWidth = 110;
-       if(im.width>normalWidth){
-       im.width=110;
-       im.height =138;}
-   }*/
-    var flag = false;
+   /* document.forms[0].action="http://ru.aliexpress.com/";*/
+
+    /*var flag = false;
     function loopImage(){
+
         var im = document.images[2];
         if(flag)im.src='img/case.jpg';
         else im.src = "img/collection.jpg";
@@ -19,3 +13,4 @@
             im.width=110;
             im.height =138;}
     }
+*/
